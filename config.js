1f6527eb4d3dd0b2e47f0bcfa5abb0eeb326594c
FGR-XD <113278821+FGR-XD@users.noreply.github.com>
2022-09-11 14:13:12 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,209 @@
+/*
+Script By Xynoz 📮
+No Enc? Chat wa.me/6283839200413
+*/
+
+import { watchFile, unwatchFile } from 'fs'
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+import moment from 'moment-timezone'
+
+/*============= WAKTU =============*/
+let wibh = moment.tz('Asia/Jakarta').format('HH')
+    let wibm = moment.tz('Asia/Jakarta').format('mm')
+    let wibs = moment.tz('Asia/Jakarta').format('ss')
+    let wktuwib = `${wibh} H ${wibm} M ${wibs} S`
+    
+    let d = new Date(new Date + 3600000)
+    let locale = 'id'
+    // d.getTimeZoneOffset()
+    // Offset -420 is 18.00
+    // Offset    0 is  0.00
+    // Offset  420 is  7.00
+    let weton = ['Pahing', 'Pon', 'Wage', 'Kliwon', 'Legi'][Math.floor(d / 84600000) % 5]
+    let week = d.toLocaleDateString(locale, { weekday: 'long' })
+    let date = d.toLocaleDateString(locale, {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric'
+    })
+      
+
+/*============== INFO OWNER ==============*/
+global.sig = 'https://instagram.com/xynoz26_'
+global.sgh = 'https://github.com/xynoz'
+global.sgc = 'https://chat.whatsapp.com/J5qUMoYUrtFKQEzXTg0Fit'
+global.swa = 'wa.me/6283839200413'
+
+/*============== PAYMENT ==============*/
+global.pdana = '082184080698'
+global.povo = '082184080698'
+global.pgopay = '083839200413'
+global.ppulsa = '083839200413'
+global.ppulsa2 = '082184080698'
+global.psaweria = 'https://saweria.co/xynozbotz'
+
+/*============== NOMOR ==============*/
+global.nomorbot = '6283839041031'
+global.nomorown = '6283839200413'
+global.namebot = 'єƖαɪηα-ʙᴏᴛᴢ'
+global.nameown = 'Ｘｙｎｏｚ'
+
+
+/*============== STAFF ==============*/
+global.owner = [
+  ['6283839200413', 'Ｘｙｎｏｚ', 'true']
+  // [number, dia creator/owner?, dia developer?]
+] // Put your number here
+global.mods = [] // Want some help?
+global.prems = [] // Premium user bukan disini nambahinnya, ketik .addprem @user 10
+
+
+/*============== API ==============*/
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  nrtm: 'https://nurutomo.herokuapp.com',
+  rey: 'https://server-api-rey.herokuapp.com',
+  xteam: 'https://api.xteam.xyz',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  lol: 'https://api.lolhuman.xyz',
+  dhnjing: 'https://dhnjing.xyz',
+  neoxr: 'https://neoxr-api.herokuapp.com',
+  zeks: 'https://api.zeks.me',
+  pencarikode: 'https://pencarikode.xyz',
+  ana: 'https://anabotofc.herokuapp.com/',
+  amel: 'https://melcanz.com',
+  hardianto: 'https://hardianto.xyz',
+  botstyle: 'https://botstyle-api.herokuapp.com',
+adiisus: 'https://adiixyzapi.herokuapp.com',
+  LeysCoder: 'https://leyscoders-api.herokuapp.com',
+  kanx: 'https://kannxapi.herokuapp.com/'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.xteam.xyz': 'cristian9407',
+  'https://anabotofc.herokuapp.com/': 'AnaBot',
+  'https://api.lolhuman.xyz': 'Xynoz',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://api.zeks.me': 'apivinz',
+  'https://pencarikode.xyz': 'pais',
+  'https://melcanz.com': 'wEDTUsWj',
+  'https://neoxr-api.herokuapp.com': 'yntkts',
+  'https://server-api-rey.herokuapp.com': 'apirey',
+  'https://botstyle-api.herokuapp.com': 'Eyar749L',
+  'https://hardianto.xyz': 'hardianto',
+  'https://leyscoders-api.herokuapp.com': 'dappakntlll'
+}
+
+/*============== WATERMARK ==============*/
+global.wm = 'єƖαɪηα-ʙᴏᴛᴢ | ʙу xуηᴏᴢ' //Main Watermark
+global.wm2 = 'єƖαɪηα-ʙᴏᴛᴢ'
+global.wm3 = 'єƖαɪηα-ʙᴏᴛᴢ | ʙу xуηᴏᴢ'
+global.botdate = `⻝ 𝗗𝗮𝘁𝗲: ${week} ${date}\n⻝ 𝗧𝗶𝗺𝗲: ${wktuwib}`
+global.bottime = `⻝ 𝗧 𝗜 𝗠 𝗘 : ${wktuwib}`
+global.titlebot = `📮 𝙀𝙡𝙖𝙞𝙣𝙖-𝘽𝙤𝙩𝙯 | By ${global.nameown}`
+global.author = 'Ｘｙｎｏｚ✍' 
+
+
+/*============== LOGO ==============*/
+global.thumb = 'https://telegra.ph/file/a94ebededebcbc792a12a.jpg' //Main Thumbnail
+global.imagebot = 'https://telegra.ph/file/a94ebededebcbc792a12a.jpg'
+global.giflogo = 'https://telegra.ph/file/a94ebededebcbc792a12a.jpg'
+global.thumbs = ['https://telegra.ph/file/a94ebededebcbc792a12a.jpg']
+global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
+
+/*============== TEXT ==============*/
+global.wait = '```𝙇𝙤𝙖𝙙𝙞𝙣𝙜...```'
+global.eror = '```𝙉𝙤𝙩 𝙁𝙤𝙪𝙣𝙙 𝟰𝟬𝟰 𝙀𝙧𝙤𝙧```'
+
+/*=========== TYPE DOCUMENT ===========*/
+global.dpptx = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+global.ddocx = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+global.dxlsx = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+global.dpdf = 'application/pdf'
+global.drtf = 'text/rtf'
+
+global.thumbdoc = 'https://telegra.ph/file/a94ebededebcbc792a12a.jpg'
+
+/*=========== FAKE SIZE ===========*/
+global.fsizedoc = '99999999999999' // default 10TB
+global.fpagedoc = '999'
+
+/*=========== HIASAN ===========*/
+// DEFAULT MENU
+global.dmenut = '❏═┅═━–〈' //top
+global.dmenub = '┊•' //body
+global.dmenub2 = '┊' //body for info cmd on Default menu
+global.dmenuf = '┗––––––––––✦' //footer
+
+// COMMAND MENU
+global.cmenut = '––––––『' //top
+global.cmenuh = '』––––––' //header
+global.cmenub = '┊☃︎ ' //body
+global.cmenuf = '┗━═┅═━––––––๑\n' //footer
+global.cmenua = '' //after
+global.pmenus = '☃︎' //pembatas menu selector
+
+global.htki = '––––––『' //hiasan title kiri
+global.htka = '』––––––' //hiasan title kanan
+global.lopr = 'Ⓟ'
+global.lolm = 'Ⓛ'
+global.htjava = '❃' //hiasan Doang :v
+
+/*============== STICKER WM ==============*/
+global.stickpack = '⛩ＸｙｎｏｚＢｏｔｚ'
+global.stickauth = `ʙʏ xʏɴᴏᴢ`
+
+global.multiplier = 38 // The higher, The harder levelup
+
+/*============== EMOJI ==============*/
+global.rpg = {
+  emoticon(string) {
+    string = string.toLowerCase()
+    let emot = {
+      level: '📊',
+      limit: '🎫',
+      health: '❤️',
+      exp: '✨',
+      money: '💹',
+      bank: '🏦',
+      potion: '🥤',
+      diamond: '💎',
+      common: '📦',
+      uncommon: '🛍️',
+      mythic: '🎁',
+      legendary: '🗃️',
+      superior: '💼',
+      pet: '🔖',
+      trash: '🗑',
+      armor: '🥼',
+      sword: '⚔️',
+      pickaxe: '⛏️',
+      fishingrod: '🎣',
+      wood: '🪵',
+      rock: '🪨',
+      string: '🕸️',
+      horse: '🐴',
+      cat: '🐱',
+      dog: '🐶',
+      fox: '🦊',
+      petFood: '🍖',
+      iron: '⛓️',
+      gold: '🪙',
+      emerald: '❇️',
+      upgrader: '🧰'
+      
+    }
+    let results = Object.keys(emot).map(v => [v, new RegExp(v, 'gi')]).filter(v => v[1].test(string))
+    if (!results.length) return ''
+    else return emot[results[0][0]]
+  }
+}
+
+//------ JANGAN DIUBAH -----
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+  unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  import(`${file}?update=${Date.now()}`)
+})
