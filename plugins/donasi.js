5832ab70b07a945a42c9722c0fd2481cd0309b67
diskahaha <109210020+diskahaha@users.noreply.github.com>
2022-09-19 00:28:11 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -2,8 +2,8 @@ import fetch from 'node-fetch'
 
 //Plugin By Xynoz!!
 let handler = async (m, { conn, usedPrefix }) => {
-	let url = 'https://telegra.ph/file/092e96af8312a94c2e4b0.jpg'
-	conn.sendButton(m.chat, '〔 Dᴏɴᴛ ғᴏʀɢᴇᴛ Dᴏɴᴀᴛɪᴏɴ 〕╭─「_• Pulsa •_」\n│ • Axis [083839200413]\n╰────\n\n╭─「_• Dana/Gopay •_」\n│ • Dana [082184080698]\n│ • Gopay [083839200413]\n╰────\n\n_◛˖ Ingin Donasi? Chat nomor owner_', wm, await(await fetch(url)).buffer(), [['Owner',`/owner`]],m)
+	let url = 'https://chat.whatsapp.com/EjczE2AmL7qHnpPbs8d74b'
+	conn.sendButton(m.chat, '〔 Dᴏɴᴛ ғᴏʀɢᴇᴛ Dᴏɴᴀᴛɪᴏɴ 〕╭─「_• Pulsa •_」\n│ • Telkom [082114579635]\n╰────\n\n╭─「_• Dana/Gopay •_」\n│ • Dana [-]\n│ • Gopay [-]\n╰────\n\n_◛˖ Ingin Donasi? Chat nomor owner_', wm, await(await fetch(url)).buffer(), [['Owner',`/owner`]],m)
 }
 handler.command = /^(donasi|dns)$/i
 handler.tags = ['info']
