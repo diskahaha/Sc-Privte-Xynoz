bef056fd1ebf5823ddbb547763d156865a2b117c
diskahaha <109210020+diskahaha@users.noreply.github.com>
2022-09-19 00:40:37 +0700

    Update group-intro.js

--- plugins/group-intro.js
+++ plugins/group-intro.js
@@ -1,18 +1,14 @@
 let handler = async (m, { conn }) => {
 
 
-let krtu = `0ཻུ۪۪ꦽꦼ̷⸙‹•══════════════♡᭄
-│       *「 Kartu Intro 」*
-│ *Nama     :* 
-│ *Gender   :* 
-│ *Umur      :* 
-│ *Hobby    :* 
-│ *Kelas      :* 
-│ *Asal         :* 
-│ *Agama    :* 
-│ *Status     :* 
-╰═════ꪶ ཻུ۪۪ꦽꦼ̷⸙ ━ ━ ━ ━ ꪶ ཻུ۪۪ꦽꦼ̷⸙
-http://bīt.ly/ᯤ
+let krtu = `
+┌–––––––––––––––––✥
+│「▰▰▰▱▱▱▱▱▱▱」
+└┬❖ 「 Kartu Intro 」
+┌┤❀  ɴᴀᴍᴀ :
+┊│❀  ᴋᴏᴛᴀ : 
+┊│❀  ᴡᴀʀɴᴀ ʙʜ ʏᴀɴɢ ᴅɪ ᴘᴀᴋᴀɪ :
+│└────────────┈
 `
 conn.fakeReply(m.chat, krtu, '0@s.whatsapp.net', wm)
 }
