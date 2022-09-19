0378c684a9231669ac1d773314dfcc6f1eb3bd4f
diskahaha <109210020+diskahaha@users.noreply.github.com>
2022-09-19 12:31:04 +0700

    Add files via upload

--- plugins/group_kick.js
+++ plugins/group_kick.js
@@ -7,25 +7,22 @@ let handler = async (m, { teks, conn, isOwner, isAdmin, args }) => {
   if(m.quoted){
 if(m.quoted.sender === ownerGroup || m.quoted.sender === conn.user.jid) return;
 let usr = m.quoted.sender;
-let nenen = await conn.groupParticipantsUpdate(m.chat, [usr], "demote"); return;
-if (nenen) m.reply(`sukses demote @${user.split('@')[0]}!`)
+await conn.groupParticipantsUpdate(m.chat, [usr], "remove"); return;
 }
-  if (!m.mentionedJid[0]) throw `tag yang mau diturunkan jabatannya!`;
+  if (!m.mentionedJid[0]) throw `tag yang mau dikick!`;
   let users = m.mentionedJid.filter(
     (u) => !(u == ownerGroup || u.includes(conn.user.jid))
   );
   for (let user of users)
     if (user.endsWith("@s.whatsapp.net"))
-      await conn.groupParticipantsUpdate(m.chat, [user], "demote");
+      await conn.groupParticipantsUpdate(m.chat, [user], "remove");
 };
 
-handler.help = ['demote @user']
-handler.tags = ['group', 'owner']
-handler.command = /^(demo?te|member|\↓)$/i
+handler.help = ['kick @user']
+handler.tags = ['group']
+handler.command = /^(kic?k|remove|tendang|\-)$/i
 
 handler.group = true
 handler.botAdmin = true
-handler.admin = true
-handler.fail = null
 
 module.exports = handler
