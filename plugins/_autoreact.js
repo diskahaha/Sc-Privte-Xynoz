f5f50a5a191c67dba165f48c642825e31199e426
diskahaha <109210020+diskahaha@users.noreply.github.com>
2022-09-27 18:27:59 +0700

    Update _autoreact.js

--- plugins/_autoreact.js
+++ plugins/_autoreact.js
@@ -2,12 +2,12 @@ let handler = async (m, { conn, text }) => {
 
     conn.sendMessage(m.chat, {
           react: {
-            text: `${pickRandom(['🧋','🧋','🧋','🧋','🧋', '🧋'])}`,
+            text: `${pickRandom(['🧃','🧃'])}`,
             key: m.key,
           }})
   
   }
-handler.customPrefix = /(bile?k|ban?h|cum?|knt?l|y?|mmk|p|b(a|i)?c?(o|i)?(t|d)?|wibu|p(a)?nt(e)?k|pepe?k|owner)/i
+handler.customPrefix = /(bile?k|ban?h)/i
 handler.command = new RegExp
   
 handler.mods = false
