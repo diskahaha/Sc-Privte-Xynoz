bff2732b484cb41a0c1423b0b186cb964c3eaf32
diskahaha <109210020+diskahaha@users.noreply.github.com>
2022-10-04 13:46:17 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,45 +1,25 @@
-let fetch = require('node-fetch')
-let handler = async (m, { conn, usedPrefix, text, command, args }) => {
-	let ftroli = {
-    key : {
-    remoteJid: 'status@broadcast',
-    participant : '0@s.whatsapp.net'
-    },
-    message: {
-    orderMessage: {
-    itemCount : 9999999,
-    status: 404,
-    surface : 404,
-    message: `❏ TIKTOK DOWNLOADER`, 
-    orderTitle: `▮Menu ▸`,
-    thumbnail: await (await fetch('https://telegra.ph/file/c2c7057129ff6f42095b8.jpg')).buffer(), //Gambarnye
-    sellerJid: '0@s.whatsapp.net' 
-    }
-    }
-    }
-    let res = await fetch(`https://botcahx-rest-api.herokuapp.com/api/dowloader/tikok?url=${args[0]}`)
-    let json = await res.json()
-    if (!json.status) return conn.sendButtonLoc(m.chat, 'https://telegra.ph/file/fd56c12d665a14793a1fb.jpg', `Harap masukkan URL sebagai parameter.\n\nContoh: ${usedPrefix + command} https://vt.tiktok.com/ZSdpHWxxG/?k=1`, wm, 'Sip', 'Ok', m)
-    let data = json.result
-    let video = data.video
-    let thumb = await (await fetch(data.thumb)).buffer()
-    let tag = `@${m.sender.split('@')[0]}`
-    conn.reply(m.chat, '*WAIT! | Mohon Tunggu Sebentar...*', m, {quoted: m, thumbnail: await (await fetch('https://telegra.ph/file/b9a32ee41970d7a71b476.jpg')).buffer(), contextInfo: { externalAdReply: {title: 'Lagi Memuat Data', sourceUrl: 'https://vt.tiktok.com/ZSdnasM19/', body: '© 𝙷𝚊𝚘𝚛𝚒𝚋𝚘𝚝𝚣 𝙱𝚢 𝚉𝚒𝚟𝚏𝚞𝚛𝚛', thumbnail: await (await fetch('https://telegra.ph/file/7d3c2136bec2eaec00f2e.jpg')).buffer(),}}})
-let txt = `Hai Kak ${tag}, Videonya Udah Jadi Nih, Kalau Mau Versi Ekstensi Lain, Pilih Dibawah Ya` 
-    await conn.sendButtonVid(m.chat, video, txt, 'Mau Ganti Ke Versi Music Klik Dibawah', `Audio`, `.tiktokaudio ${args[0]}`, 0, { quoted: ftroli,
-    contextInfo: { forwardingScore: 99999, isForwarded: true,
-        externalAdReply: {
-        	sourceUrl: 'https://vt.tiktok.com/ZSRRmS8vh/',
-            title: 'Tiktok Downloader 🎥',
-            body: wm2,
-          thumbnail: thumb
-        }
-     }
-    })
- } 
-handler.help = ['tiktok'].map(v => v + ' <url>')
+import { tiktokdl, tiktokdlv2, tiktokdlv3 } from '@bochilteam/scraper'
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
+    const { author: { nickname }, video, description } = await tiktokdl(args[0])
+        .catch(async _ => await tiktokdlv2(args[0]))
+        .catch(async _ => await tiktokdlv3(args[0]))
+    const url = video.no_watermark2 || video.no_watermark || 'https://tikcdn.net' + video.no_watermark_raw || video.no_watermark_hd
+    if (!url) throw 'Can\'t download video!'
+    conn.sendHydrated(m.chat, `${htki} ᴛɪᴋᴛᴏᴋ ᴡᴍ ${htka}`, `➔ ɴɪᴄᴋɴᴀᴍᴇ ${nickname}${description ? `\n➔ ᴅᴇsᴄʀɪᴘᴛɪᴏɴ:\n${description}` : ''}`, await (await fetch(url)).buffer(),
+        url, '🌎 s ᴏ ᴜ ʀ ᴄ ᴇ', null, null, [
+        ['🌸ᴅᴏɴᴀꜱɪ', `.donasi`],
+        ['🌹ᴀᴜᴅɪᴏ', `.tiktokaudio ${args}`],
+        [null, null]], m)
+}
+handler.help = ['tiktok', 'tiktok', 'tiktokdl'].map(v => v + ' <url>')
 handler.tags = ['downloader']
+handler.command = /^(tik(tok)?(tok)?(dl)?)$/i
 
-handler.command = /^((tt|tiktok)?(dl)?)$/i
+handler.limit = true
 
-module.exports = handler
+export default handler
+
+//Plugin By Xynoz!! Jangan Lupa Subscribe?!
