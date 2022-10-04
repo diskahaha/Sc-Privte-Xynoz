4e703619979cd5cfdad9ba93853530c3b76050d9
diskahaha <109210020+diskahaha@users.noreply.github.com>
2022-10-04 13:24:38 +0700

    Update tool-translate.js

--- plugins/tool-translate.js
+++ plugins/tool-translate.js
@@ -1,4 +1,4 @@
-import translate from 'translate-google-api'
+const translate = require('translate-google-api')
 const defaultLang = 'en'
 const tld = 'cn'
 
@@ -39,5 +39,7 @@ Daftar bahasa yang didukung: https://cloud.google.com/translate/docs/languages
 handler.help = ['translate'].map(v => v + ' <lang> <teks>')
 handler.tags = ['tools']
 handler.command = /^(tr(anslate)?)$/i
-
-export default handler
+handler.limit = false
+handler.fail = null
+handler.exp = 0
+module.exports = handler
